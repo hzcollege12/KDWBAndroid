160623cb9751601645b2458cc91b2f1aa1353a65
st01 <st01@teh>
2013-11-21 16:05:26 +0800

    KEHUA 2014/11/21

--- KDWBAndroid/src/wyf/wpf/PublishDiaryActivity.java
+++ KDWBAndroid/src/wyf/wpf/PublishDiaryActivity.java
@@ -1,3 +1,35 @@
+ package wyf.wpf;
+ import static wyf.wpf.ConstantUtil.SERVER_ADDRESS;
+ ...
+ import android.widet.Toast;
+ public class PublishDiaryActivity extends Activity{
+       MyConnector mc=null;
+       ProgressDialog pd= null;
+       string uno = null;
+       protected void onCreate(Bundle savedInstanceState){
+       super.onCreate(savedInstanceState);
+       Intent intent= getIntent();
+       uno= intent.getStringExtra("uno");
+       setContentView(R.layout.publish_diary);
+       Button btnDiay=(Button)findViewByld(R.id.btnDiary);
+       btnDiary.setOnClickListener(newView.OnClickListener(){
+                public void onClick(View v) {
+               publishDiary();
+           }
+   });
+    Button btn DiaryBack=(Button)findViewByld(R.id.btnDiaryBack);
+    btnDiaryBack.setOnClickListener(new View.OnClickListener(){
+         pubic void onClick(View v){
+          finish();
+       }
+   });
+ }
+   public void publishDiary(){
+   protected void onDestroy(){
+}
+
+
+
 //P187 Íõ´ïºê
 
  public void publishDiary(){
